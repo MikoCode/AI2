d7cbb40f1c817ab632c1f3f3c46aacd0ed9fabc7
Miko <mickkaczmarczyk@gmail.com>
2024-07-06 11:39:37 +0200

    AI2

---
+++ ProgramowanieAI2/MazeAndPathFinding-PrAI2-main/Assets/Scripts/GameManager.cs
@@ -0,0 +1,159 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    
+
+    public States state;
+
+  
+    private MazeGenerator _mazeGenerator;
+
+    [Header("GameObjects")]
+    [SerializeField] private GameObject SeekerPrefab;
+    [SerializeField] private GameObject EndPointPrafab;
+    [NonSerialized] public GameObject lastSeeker;
+    [NonSerialized]public List<Seeker> seekers = new List<Seeker>(); //all placed seekers
+    private GameObject mainSeeker;
+
+    [Header("State Materials")] 
+    [SerializeField] private Material pathMaterial;
+    [SerializeField] private Material mainSeekerMaterial;
+    [SerializeField] private Material disabled;
+    [SerializeField] public Material stuck;
+
+    [Header("ShowPath")] 
+    public List<Vector3> pathSeekers = new List<Vector3>(); //shortest path from A to B
+
+    [FormerlySerializedAs("sphere")]
+    [Header("Mover")] 
+    [SerializeField] private GameObject mover;
+    
+    //vars
+    public Vector3 pointA;
+    public Vector3 pointB;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        _mazeGenerator = GetComponent<MazeGenerator>();
+        StartState(States.GenerateMaze);
+    }
+
+    public void StartState(States newState)
+    {
+        switch (newState)
+        {
+            case States.GenerateMaze:
+                state = States.GenerateMaze;
+                GetComponent<MazeGenerator>().StartMaze();
+                break;
+            case States.SetTwoRandomPoints:
+                state = States.SetTwoRandomPoints;
+                SetPoints();
+                break;
+            case States.FindPathBetweenPoints:
+                state = States.FindPathBetweenPoints;
+                InstantiatePathFinders();
+                break;
+            case States.Finish:
+                state = States.Finish;
+                DisableSeekers();
+                ShowPath();
+                break;
+            case States.MoveObject:
+                StartMover();
+                break;
+            default:
+                Debug.LogWarning("Tragic accident");
+                break;
+        }
+    }
+
+    private void StartMover()
+    {
+        Vector3 mainSeekerPos = mainSeeker.transform.position;
+        mover.transform.position = new Vector3(mainSeekerPos.x, mainSeekerPos.y + mover.GetComponent<MoveSphere>().AddFixedYPos, mainSeekerPos.z);
+        mover.SetActive(true);
+        StartCoroutine(mover.GetComponent<MoveSphere>().MoveTowardsPoint(pathSeekers));
+    }
+    
+    private void DisableSeekers()
+    {
+        foreach (var seeker in seekers)
+        {
+            seeker.enabled = false;
+            seeker.GetComponent<MeshRenderer>().material = disabled;
+        }
+    }
+
+    private void ShowPath()
+    {
+        ChangeMaterialToShowPath(lastSeeker);
+        pathSeekers.Add(lastSeeker.transform.position);
+        while(lastSeeker.GetComponent<StorePreviousSeeker>().previousSeeker != null)
+        {
+            lastSeeker = lastSeeker.GetComponent<StorePreviousSeeker>().previousSeeker;
+            pathSeekers.Add(lastSeeker.transform.position);
+            if(GameObject.ReferenceEquals(lastSeeker,mainSeeker)) break;
+            ChangeMaterialToShowPath(lastSeeker);
+        }
+        
+        StartState(States.MoveObject);
+    }
+
+    private void ChangeMaterialToShowPath(GameObject obj)
+    {
+        obj.GetComponent<MeshRenderer>().material = pathMaterial;
+    }
+    
+    private void InstantiatePathFinders()
+    {
+
+        mainSeeker = Instantiate(SeekerPrefab, pointA, Quaternion.identity);
+        mainSeeker.GetComponent<Seeker>().FillInfo(SeekerPrefab,true,null);
+        mainSeeker.GetComponent<MeshRenderer>().material = mainSeekerMaterial;
+     
+        Instantiate(EndPointPrafab, pointB, Quaternion.identity);
+    }
+
+    private void SetPoints()
+    {
+        Vector3[] corners = _mazeGenerator.Corners;
+        float yPos = _mazeGenerator.FixedYPos;
+        pointA = new Vector3((int)Random.Range(corners[0].x, corners[1].x), yPos, (int)Random.Range(corners[0].z, corners[1].z));
+        pointB = new Vector3((int)Random.Range(corners[0].x, corners[1].x), yPos, (int)Random.Range(corners[0].z, corners[1].z));
+
+        pointA.z += 0.55f; 
+        pointB.z += 0.55f;
+        
+        StartState(States.FindPathBetweenPoints);
+    }
+}
+
+public enum States
+{
+    GenerateMaze,
+    SetTwoRandomPoints,
+    FindPathBetweenPoints,
+    Finish,
+    MoveObject
+}
+
+public enum PathFindingEnum
+{
+    Seeker,
+    EndPoint
+}
---
+++ ProgramowanieAI2/MazeAndPathFinding-PrAI2-main/Assets/Scripts/PathFinding/Seeker.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Seeker : MonoBehaviour
+{
+    private SeekerStates currentState = SeekerStates.AbleToSeek;
+    
+    private GameObject seekerPrefab;
+    private bool mainSeeker;
+    
+    private InputSeeker _inputSeeker;
+    private InputAction map;
+
+    public void FillInfo(GameObject prefab, bool godSeeker, GameObject prevSeeker)
+    {
+        seekerPrefab = prefab;
+        mainSeeker = godSeeker;
+        GetComponent<StorePreviousSeeker>().previousSeeker = prevSeeker;
+    }
+    
+    private void Start()
+    {
+        _inputSeeker = new InputSeeker();
+        _inputSeeker.MapaAkcji.Enable();
+        map = _inputSeeker.MapaAkcji.Akcja;
+        map.performed += CheckNext;
+    }
+
+    private void OnDisable()
+    {
+        if(map != null) map.performed -= CheckNext;
+    }
+
+    public void CheckNext(InputAction.CallbackContext ctx)
+    {
+        if(!ctx.performed) return;
+        
+        if (currentState == SeekerStates.AbleToSeek)
+        {
+            MoveTo(transform.position + Vector3.forward);
+            MoveTo(transform.position + Vector3.back);
+            MoveTo(transform.position + Vector3.left);
+            MoveTo(transform.position + Vector3.right);
+        }
+        
+        if (GameManager.Instance.state != States.Finish && !mainSeeker) GetComponent<MeshRenderer>().material = GameManager.Instance.stuck;
+        currentState = SeekerStates.UnableToSeek;
+    }
+
+    private void MoveTo(Vector3 movePos)
+    {
+        Vector3 dir = movePos - transform.position;
+        Ray ray = new Ray(transform.position, dir);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 1f))
+        {
+            switch (hit.collider.gameObject.tag)
+            {
+                case "Wall":
+                    
+                    return;
+                case "EndPoint":
+                    GameManager.Instance.lastSeeker = gameObject;
+                    GameManager.Instance.StartState(States.Finish);
+                    return;
+            }
+        }
+        else
+        {
+            GameObject seeker = Instantiate(seekerPrefab, movePos, Quaternion.identity);
+            seeker.GetComponent<Seeker>().FillInfo(seekerPrefab, false, gameObject);
+            GameManager.Instance.seekers.Add(seeker.GetComponent<Seeker>());
+        }
+    }
+}
+
+public enum SeekerStates
+{
+    AbleToSeek,
+    UnableToSeek
+}
